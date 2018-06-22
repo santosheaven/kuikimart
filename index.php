0094ea10553b0596181801b1205354579587c234
Asus <Asus@192.168.1.10>
2018-06-21 22:40:55 -0500

    First Commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" >
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" ></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>        
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" ></script>
+        <title>KuikiMart te damos lo mejor!</title>
+    </head>
+    <body>
+        <div id="page">
+            <?php include_once 'header.php'; ?>
+            <?php include_once 'content.php'; ?>
+            <?php include_once 'footer.php'; ?>
+        </div>
+    </body>
+</html>
